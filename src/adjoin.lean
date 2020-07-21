e2c5eb54a8f1166cc7d04827ba3d6d74060f1788
Thomas <thomas.l.browning@gmail.com>
2020-07-21 14:24:04 -0700

    adjoining an element

--- src/adjoin.lean
+++ src/adjoin.lean
@@ -1,1 +1,7 @@
-/- Adjoining an element. -/
+import field_theory.subfield
+import ring_theory.algebra
+
+variables (F : Type*) [field F] (K : Type*) [field K] [algebra F K]
+
+--a little hacky (maybe there's a better way)
+definition adjoin [field F] [field K] [algebra F K] (α : K) : set K := field.closure ((⊤ : set K) ∪ {α})
