018d7155e487df79116f39aceec7814f989c63f8
Thomas <thomas.l.browning@gmail.com>
2020-07-21 17:13:50 -0700

    some definitions

--- src/adjoin.lean
+++ src/adjoin.lean
@@ -1,7 +1,8 @@
 import field_theory.subfield
 import ring_theory.algebra
 
-variables (F : Type*) [field F] (K : Type*) [field K] [algebra F K]
+/- adjoining an element -/
 
 --a little hacky (maybe there's a better way)
-definition adjoin [field F] [field K] [algebra F K] (α : K) : set K := field.closure ((⊤ : set K) ∪ {α})
+definition adjoin (F : Type*) [field F] (K : Type*) [field K] [h : algebra F K] (α : K) : set K :=
+field.closure (set.range (algebra_map F K) ∪ {α})
---
+++ src/separable.lean
@@ -0,0 +1,5 @@
+import ring_theory.algebra
+
+/- separable extension -/
+
+definition separable (F : Type*) [field F] (K : Type*) [field K] [h : algebra F K] : Prop := sorry
