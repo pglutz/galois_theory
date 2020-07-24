5236c43505d757b8ab94aed7c973906acfde40fd
amichaelsen <anya.michaelsen2@gmail.com>
2020-07-24 13:37:11 -0700

    removed patrick's two lines

--- src/primitive_element.lean
+++ src/primitive_element.lean
@@ -3,8 +3,6 @@ import separable
 
 /- Prove the primitive element theorem. -/
 
-
-
 variables (F : Type*) [field F] (K : Type*) [field K] [algebra F K]
 
 theorem primitive_element : separable F K → (∃ α : K, adjoin F K α = (⊤ : set K)) := sorry
