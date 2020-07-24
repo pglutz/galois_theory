1626e5350f2fb6d7972f8a89dc97f491c2e341d7
Thomas <thomas.l.browning@gmail.com>
2020-07-24 14:12:04 -0700

    separable_extension definition

--- src/separable.lean
+++ src/separable.lean
@@ -4,9 +4,8 @@ import field_theory.separable
 
 /- separable extension -/
 
-variables (F : Type*) [field F] (E : Type*) [field E] [h : algebra F E] (α : E)
-
-definition separable_element (F : Type*) [field F] (E : Type*) [field E] [algebra F E] (α : E) : Prop :=
+definition separable_element (F : Type*) [field F] {E : Type*} [field E] [algebra F E] (α : E) : Prop :=
 ∃ (h1 : is_integral F α), (polynomial.separable (minimal_polynomial h1))
 
-definition separable_extension (F : Type*) [field F] (E : Type*) [field E] [algebra F E] : Prop := sorry
+definition separable_extension (F : Type*) [field F] (E : Type*) [field E] [algebra F E] : Prop :=
+∀ α : E, separable_element F α
