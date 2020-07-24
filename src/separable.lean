ad2f3f4e598b70e7acead98e97cc5e9f301a1000
Thomas <thomas.l.browning@gmail.com>
2020-07-24 14:07:03 -0700

    definition of separable element

--- src/separable.lean
+++ src/separable.lean
@@ -1,5 +1,12 @@
 import ring_theory.algebra
+import field_theory.minimal_polynomial
+import field_theory.separable
 
 /- separable extension -/
 
-definition separable (F : Type*) [field F] (E : Type*) [field E] [h : algebra F E] : Prop := sorry
+variables (F : Type*) [field F] (E : Type*) [field E] [h : algebra F E] (α : E)
+
+definition separable_element (F : Type*) [field F] (E : Type*) [field E] [h : algebra F E] (α : E) : Prop :=
+∃ (h1 : is_integral F α), (polynomial.separable (minimal_polynomial h1))
+
+definition separable_extension (F : Type*) [field F] (E : Type*) [field E] [h : algebra F E] : Prop := sorry
