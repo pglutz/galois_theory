584d7ac5380243b7aa4e51cb87bbed2a550ee62a
Thomas <thomas.l.browning@gmail.com>
2020-07-27 08:13:55 -0700

    comment mentioning that mathlib might be getting 'is_separable

--- src/separable.lean
+++ src/separable.lean
@@ -7,5 +7,6 @@ import field_theory.separable
 definition separable_element (F : Type*) [field F] {E : Type*} [field E] [algebra F E] (α : E) : Prop :=
 ∃ (h : is_integral F α), (polynomial.separable (minimal_polynomial h))
 
+--actually, a new version of mathlib might have "is_separable F E" in field_theory.separable
 definition separable_extension (F : Type*) [field F] (E : Type*) [field E] [algebra F E] : Prop :=
 ∀ α : E, separable_element F α
