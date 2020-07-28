814bf11bba45ec791070cab8c33d1c271849d926
Jordan Brown <jordanlbrown@MacBook-Pro.localdomain>
2020-07-28 14:41:48 -0700

    fixed the statement of the minimal polynomial quotient isomorphism

--- src/root_construction.lean
+++ src/root_construction.lean
@@ -2,4 +2,4 @@ import adjoin_simple
 import ring_theory.adjoin_root
 
 theorem adjunction_equiv_to_quotient  (F : Type*) [field F] (E : Type*) [field E] [algebra F E] (α : E) : 
-∃ (f:polynomial F) (isom: (adjoin_root f) ≃+* (adjoin_simple F E α )), (irreducible f ∧ isom.to_fun adjoin_root.root = α ) := sorry 
+∃ (f:polynomial F) (isom: (adjoin_root f) ≃+* (adjoin_simple F E α )), (irreducible f ∧ ↑ (isom.to_fun (adjoin_root.root f)) = α ) := sorry 
