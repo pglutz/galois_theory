e4bcfbd3f3c0c7619dba71cf7bfa08ca2b42e489
Rahul Dalal <Rahul@Rahuls-MacBook-Pro-2.local>
2020-07-28 18:38:09 -0400

    changed variables in primitive element to {}

--- src/primitive_element.lean
+++ src/primitive_element.lean
@@ -6,7 +6,7 @@ open finite_dimensional
 
 /- Prove the primitive element theorem. -/
 
-variables (E : Type*) [field E] (F : Type*) [field F] [algebra F E]
+variables {E : Type*} [field E] {F : Type*} [field F] [algebra F E]
 
 --This might end up being changed slightly. At some point we will need to finalize this
 
@@ -15,7 +15,7 @@ variables (E : Type*) [field E] (F : Type*) [field F] [algebra F E]
 --This might end up being changed slightly. At some point we will need to finalize this
 theorem primitive_element_two (α β : E) : separable_extension F E → finite_dimensional F E → (∃ γ : E, adjoin F E {α, β} = adjoin F E {γ} ) := sorry
 
-theorem primitive_element_aux (hs : is_separable F E) (hfd: finite_dimensional F E) : ∀ n : ℕ, findim F E = n → (∃ α : E, adjoin F E {α} = (⊤ : set E)) 
+private theorem primitive_element_aux (hs : is_separable F E) (hfd: finite_dimensional F E) : ∀ n : ℕ, findim F E = n → (∃ α : E, adjoin F E {α} = (⊤ : set E)) 
 | 0 := sorry
 | 1 := 
 begin
@@ -26,4 +26,4 @@ begin
 sorry
 end
 
-theorem primitive_element (hs : is_separable F E)  (hfd : finite_dimensional F E) : (∃ α : E, adjoin F E {α} = (⊤ : set E)) := primitive_element_aux _ _ hs hfd (findim F E) rfl
+theorem primitive_element (hs : is_separable F E)  (hfd : finite_dimensional F E) : (∃ α : E, adjoin F E {α} = (⊤ : set E)) := primitive_element_aux hs hfd (findim F E) rfl
