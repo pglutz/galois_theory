31a0ec2ca04cae3fba9a4b74f477c07613775582
Thomas <thomas.l.browning@gmail.com>
2020-07-28 12:33:11 -0700

    potential new mathlib definition is_separable

--- src/separable.lean
+++ src/separable.lean
@@ -5,8 +5,11 @@ import field_theory.separable
 /- separable extension -/
 
 definition separable_element (F : Type*) [field F] {E : Type*} [field E] [algebra F E] (α : E) : Prop :=
-∃ (h : is_integral F α), (polynomial.separable (minimal_polynomial h))
+∃ (h : is_integral F α), (minimal_polynomial h).separable
 
---actually, a new version of mathlib might have "is_separable F E" in field_theory.separable
 definition separable_extension (F : Type*) [field F] (E : Type*) [field E] [algebra F E] : Prop :=
 ∀ α : E, separable_element F α
+
+--actually, a new version of mathlib might have this definition of is_separable
+@[class] def is_separable (F K : Sort*) [field F] [field K] [algebra F K] : Prop :=
+∀ x : K, ∃ H : is_integral F x, (minimal_polynomial H).separable
