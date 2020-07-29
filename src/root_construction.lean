09e760a4b4acbcbd325d98799ea144f80d792f01
Jordan Brown <jordanlbrown@MacBook-Pro.localdomain>
2020-07-29 16:31:26 -0700

    updated statement

--- src/root_construction.lean
+++ src/root_construction.lean
@@ -1,5 +1,19 @@
 import adjoin_simple
 import ring_theory.adjoin_root
 
-theorem adjunction_equiv_to_quotient  (F : Type*) [field F] (E : Type*) [field E] [algebra F E] (α : E) : 
-∃ (f:polynomial F) (isom: (adjoin_root f) ≃+* (adjoin_simple F E α )), (irreducible f ∧ ↑ (isom.to_fun (adjoin_root.root f)) = α ) := sorry 
+/-   lemma bijective_ring_homorphism_is_an_isomorphism (R:Type*) [ring R] (S:Type*) [ring S] (φ:R→ S)-/
+
+theorem adjunction_equiv_to_quotient  (F : Type*) [field F] (E : Type*) [field E] [algebra F E] (α : E) (h : is_integral F α): 
+∃ (f:polynomial F) (hom: (adjoin_root f) →+* (adjoin_simple F E α )), 
+(irreducible f ∧ ∃ (isom: (adjoin_root f) ≃+* (adjoin_simple F E α )),(isom.to_fun=hom ∧ ↑ (isom.to_fun (adjoin_root.root f)) = α )) := 
+begin
+    let f:=minimal_polynomial h,
+    use f,
+    use the_map F E α h,
+    split,
+    exact minimal_polynomial.irreducible h,
+    sorry
+    
+
+
+end
