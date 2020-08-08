27fdf8c5bd471a8e0352b709ba75b37a53db3d02
Thomas <thomas.l.browning@gmail.com>
2020-08-07 22:17:44 -0700

    working on primitive_element_two_inf_key

--- src/primitive_element.lean
+++ src/primitive_element.lean
@@ -161,15 +161,6 @@ theorem primitive_element_fin [fintype F] (hfd : finite_dimensional F E) :
 lemma primitive_element_two_aux (F : set E) [is_subfield F] (α β : E) (f g : polynomial F) (F_inf : F.infinite) :
     ∃ c : F, c ≠ 0 ∧ ∀ (β' : roots F E g) (α' : roots F E f), β ≠ β' → α + c*β ≠ α' + c*β' := sorry
 
-lemma primitive_element_two_inf_key_aux (F : set E) [is_subfield F] (F_inf : F.infinite) (α β : E)
-(hα : is_integral F α) (hβ : is_integral F β) (h_sep : (minimal_polynomial hα).separable)
-(hf : polynomial.splits (algebra_map F E) (minimal_polynomial hα))
-(hg : polynomial.splits (algebra_map F E) (minimal_polynomial hβ)) :
-∃ c : F, β ∈ F[α + c*β] :=
-begin
-    sorry
-end
-
 lemma primitive_element_two_inf_key (F : set E) [is_subfield F] (α β : E) (F_sep : is_separable F E)
     (F_inf : F.infinite) : ∃ c : F, β ∈ F[α + c*β] :=
 begin
@@ -177,7 +168,10 @@ begin
     rcases F_sep β with ⟨hβ, hg⟩,
     let f := minimal_polynomial hα,
     let g := minimal_polynomial hβ,
-    let f_E := f.map(algebra_map F E),
+    let E' := polynomial.splitting_field (g.map (algebra_map F E)),
+
+
+    /-let f_E := f.map(algebra_map F E),
     let g_E := g.map(algebra_map F E),
     let E' := polynomial.splitting_field (f_E * g_E),
     let α' := algebra_map E E' α,
@@ -186,14 +180,28 @@ begin
     let f'' := f.map(algebra_map F F'),
     let g'' := g.map(algebra_map F F'),
     have F'_inf : F'.infinite := by apply inclusion.infinite (set.infinite_coe_iff.2 F_inf),
-    have hα' : is_integral F' α' := sorry,
+    have hα' : is_integral F' α',
+    dsimp[α'],
     have hβ' : is_integral F' β' := sorry,
-    have hα'_sep : (minimal_polynomial hα').separable,
-    have key := primitive_element_two_inf_key_aux E' F' F'_inf α' β' hα' hβ',
-    sorry,
-    rcases primitive_element_two_aux K F' α' β' f'' g'' F'_inf with ⟨c, c_ne_0, hc⟩,
+    have hα'_sep : (minimal_polynomial hα').separable := sorry,
+    have hf : polynomial.splits (algebra_map F' E') (minimal_polynomial hα') := sorry,
+    have hg : polynomial.splits (algebra_map F' E') (minimal_polynomial hβ') := sorry,
+    have key := primitive_element_two_inf_key_aux E' F' F'_inf α' β' hα' hβ' hα'_sep hf hg,
+    cases key with c hc,
+    use (inclusion_isomorphism F E').symm c,-/
+    --have swap : algebra_map E E' (α + (inclusion_isomorphism F E').symm c * β) = α' + c * β',
+    --dsimp[α',β'],
+    --rw ring_hom.map_add,
+    --rw ring_hom.map_mul,
+
+
+
+
+    /-sorry,
+    rcases primitive_element_two_aux E' F' α' β' f'' g'' F'_inf with ⟨c, c_ne_0, hc⟩,
     replace c_ne_0 : (c : K) ≠ 0 := ne_zero_of_ne_zero F' K c_ne_0,
-    sorry,
+    sorry,-/
+    sorry
 end
 
 /-- Primitive element theorem for adjoining two elements to an infinite field. -/
