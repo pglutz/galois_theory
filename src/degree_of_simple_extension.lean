bb5cc536dabbe20d80aa806a72ee5057952b8310
Jordan Brown <jordanlbrown@MacBook-Pro.localdomain>
2020-09-25 13:51:52 -0700

    shows that the polynomial quotient map is a module

--- src/degree_of_simple_extension.lean
+++ src/degree_of_simple_extension.lean
@@ -9,49 +9,23 @@ import data.zmod.basic
 import data.polynomial.basic
 import adjoin
 
+noncomputable theory
+local attribute [instance, priority 100] classical.prop_decidable
+
 variables (F : Type*) [field F] {E : Type*} [field E] [algebra F E] (S : set E)(α : E) (h : is_integral F α)
 
-instance algebra_is_vector_space (V:Type*)[add_comm_group V][semiring V][algebra F V]: vector_space F V:=sorry
+
+
+lemma smul3:
+∀  (p:polynomial F) (c_1 : F)  (f: polynomial F), (adjoin_root.mk p) (c_1 • f) = c_1 • ((adjoin_root.mk p) f):=λ p c f, by simpa[algebra.smul_def,ring_hom.map_mul]
 
 
 
-lemma adjunction_degree_finite : finite_dimensional F (adjoin_root (minimal_polynomial h)) :=
-begin
-    let minimal:=minimal_polynomial h,
-    let degree:=polynomial.nat_degree minimal,
-    let x:polynomial F:= polynomial.X,
-    let S:= {n: ℕ| n<degree},
-    let η := λ (n:S), adjoin_root.mk minimal (x^(n:ℕ)),
-    let ν:= λ (n:S), (x^(n:ℕ)),
-    have comp: η = (adjoin_root.mk minimal) ∘ ν, 
-    exact rfl,
+noncomputable def module_quotient_map (p:polynomial F): polynomial F →ₗ[F] adjoin_root p :=
 
+{
     
-    have is_fin:fintype S,
-    exact set.fintype_lt_nat degree,
-
-    have basis:is_basis F η,
-    unfold is_basis,
-    split,
-    apply linear_independent_iff.2,
-    intros l eq_zero,
-    have decomp: (finsupp.total ↥S (adjoin_root minimal) F η) l=(adjoin_root.mk minimal) ((finsupp.total ↥S (polynomial F) F ν) l),
-    rw comp,
-    
-    
-    /-have is_ideal:=ideal.quotient.eq_zero_iff_mem.1 eq_zero,-/
-
-
-    
-
-end
-
-
-/-  apply finite_dimensional.of_finite_basis, -/
-
-lemma quotient_degree : (finite_dimensional.findim F (adjoin_root (minimal_polynomial h))) = (minimal_polynomial h).nat_degree :=
-
-begin
-    sorry
-
-end
+    to_fun:=(adjoin_root.mk p).to_fun, 
+    map_add':=(adjoin_root.mk p).map_add,
+    map_smul':=smul3 F p    
+}
