8227d09e46884fb31f0552edb9d4668b53176d08
Thomas Browning <tb65536@uw.edu>
2020-10-09 13:53:50 -0700

    new attempt

--- src/degree_of_simple_extension.lean
+++ src/degree_of_simple_extension.lean
@@ -29,6 +29,24 @@ def module_quotient_map : polynomial F →ₐ[F] adjoin_root p :=
 
 def canonical_basis: {n: ℕ| n<polynomial.nat_degree p }→ adjoin_root p:= λ (n:{n: ℕ| n<polynomial.nat_degree p }), adjoin_root.mk p (polynomial.X^(n:ℕ))
 
+lemma canonical_basis_is_basis : is_basis F (canonical_basis p) :=
+begin
+  split,
+  { apply linear_independent_iff.mpr,
+    intros f hf,
+    dsimp only [canonical_basis] at hf,
+    dsimp only [finsupp.total] at hf,
+    rw finsupp.lsum_apply at hf,
+    dsimp [finsupp.sum] at hf,
+    simp only [linear_map.id_coe, id.def] at hf,
+    change f.support.sum (λ a, (f a) • (module_quotient_map p (X ^ ↑a))) = 0 at hf,
+    simp only [←alg_hom.map_smul] at hf,
+    haveI : is_add_monoid_hom (module_quotient_map p) := sorry,
+    rw finset.sum_hom at hf,
+    sorry,
+     },
+  {},
+end
 
 
  
